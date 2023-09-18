c325192b4f09c120c083630a936b1fa4eed29158
Matea Ilišević <milisevic@foi.hr>
2023-09-18 13:44:25 +0200

    setuped simple database for user

---
+++ FlightAppDatabase/dbo/Stored Procedures/InsertTestUsers.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE InsertTestUsers
+AS
+BEGIN
+    -- User1
+    DECLARE @Password1 NVARCHAR(50) = 'password1';
+    INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [PasswordSha])
+    VALUES (1, 'user1', 'user1@example.com', HASHBYTES('SHA2_256', @Password1)); 
+    
+    -- User2
+    DECLARE @Password2 NVARCHAR(50) = 'password2';
+    INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [PasswordSha])
+    VALUES (2, 'user2', 'user2@example.com', HASHBYTES('SHA2_256', @Password2));
+    
+    -- User3
+    DECLARE @Password3 NVARCHAR(50) = 'password3';
+    INSERT INTO [dbo].[User] ([UserId], [Username], [Email], [PasswordSha])
+    VALUES (3, 'user3', 'user3@example.com', HASHBYTES('SHA2_256', @Password3));
+END;
---
+++ FlightAppDatabase/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserId]      INT           NOT NULL,
+    [Username]    NVARCHAR (50) NOT NULL,
+    [Email]       NVARCHAR (50) NOT NULL,
+    [PasswordSha] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC),
+    UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
