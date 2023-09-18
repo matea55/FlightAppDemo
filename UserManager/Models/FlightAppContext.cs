f4bc789a6587dfdb31ce45ee00d6c8ba25463200
Matea Ilišević <milisevic@foi.hr>
2023-09-18 15:44:29 +0200

    added context

---
+++ UserManager/Models/FlightAppContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManager.Models;
+
+public partial class FlightAppContext : DbContext
+{
+    public FlightAppContext()
+    {
+    }
+
+    public FlightAppContext(DbContextOptions<FlightAppContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Initial Catalog=FlightApp;Trusted_Connection=true;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__User__1788CC4C057F1AE8");
+
+            entity.ToTable("User");
+
+            entity.HasIndex(e => e.Username, "UQ__User__536C85E431719D48").IsUnique();
+
+            entity.Property(e => e.UserId).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.PasswordSha).HasMaxLength(64);
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
